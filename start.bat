073ab4ab725aa121b672e4ba462a9dd05464a62f
under <under.64@gmail.com>
2018-11-11 01:21:12 +0100

    feat: open browser for auto start feature

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 mvnw.cmd package -DskipTests & ^
 START /B java -jar -Dspring.config.location=target/ target/bot.jar & ^
 cd front/ & ^
-START /B npm run dev
+START /B npm run dev & ^
+START "" http://localhost:3000
